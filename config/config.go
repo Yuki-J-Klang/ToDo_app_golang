f6b0241fc264f4279c533630185aca6a08e166dc
Yuki-J-Klang <Yuki.i.000915@gmail.com>
2022-12-02 23:30:43 +0900

    add:users.go

--- config/config.go
+++ config/config.go
@@ -28,7 +28,7 @@ func LoadConfig() {
 	}
 	Config = ConfigList{
 		Port:      cfg.Section("web").Key("port").MustString("8080"),
-		SQLDriver: cfg.Section("dm").Key("driver").String(),
+		SQLDriver: cfg.Section("db").Key("driver").String(),
 		DbName:    cfg.Section("db").Key("name").String(),
 		LogFile:   cfg.Section("web").Key("logfile").String(),
 	}
--- go.mod
+++ go.mod
@@ -7,4 +7,7 @@ require (
 	gopkg.in/go-ini/ini.v1 v1.67.0
 )
 
-require github.com/stretchr/testify v1.8.1 // indirect
+require (
+	github.com/google/uuid v1.3.0 // indirect
+	github.com/stretchr/testify v1.8.1 // indirect
+)
